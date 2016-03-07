f1864768b83e04fe1f63f7df1c6c7602c1e65335
LandGod <tentativechaos@gmail.com>
2016-03-07 14:01:40 -0800

    Again, just committing this because I've done a lot of crap, not because there is any particular milestone here.
    -Added a lot to StartupDialog.
    
    -Added a Notes file.

--- othello_gui.py
+++ othello_gui.py
@@ -10,6 +10,16 @@ import tkinter
 
 
 DEFAULT_FONT = ('Helvetica', 14)
+TITLE_FONT = ('Helvetica', 18)
+SUBTITLE_FONT = ('Helvetica', 16)
+
+
+class UserQuit(Exception):
+    """To be thrown when the user elects to quit the program."""
+
+
+class UnableToIdentifyWidget(Exception):
+    """To be thrown when the tkinter widget specified cannot be identified as one of the expected types by a function"""
 
 
 class OthelloApplication:
@@ -19,7 +29,7 @@ class OthelloApplication:
 
         self._root_window = tkinter.Tk()
 
-        self._canvas = tkinter.Canvas( master=self._root_window, width=800, height=800, background='#0082b')
+        self._canvas = tkinter.Canvas( master=self._root_window, width=800, height=800, background='#00802b')
 
         self._canvas.grid(row=0, column=0, padx=10, pady=10, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
 
@@ -30,25 +40,11 @@ class OthelloApplication:
         self._root_window.rowconfigure(0, weight=1)
         self._root_window.columnconfigure(0, weight=1)
 
-        dialog_info = None
-        dialog_error = None
+        startup = StartupDialog()
+        startup.show()
 
-        while True:
-            startup = StartupDialog()
-            startup.show()
-
-            if startup.was_ok_clicked:
-
-                dialog_error, dialog_info = self._validate_startup_parameters(startup)
-                if dialog_error is not None:
-                    continue
-
-                self.state = othello_logic.GameState(dialog_info[0], dialog_info[1], dialog_info[2], dialog_info[3],
-                                                     dialog_info[4])
-
-
-
-
+        state = othello_logic.GameState(startup.rows(), startup.cols(), startup.first(), startup.ne_player(),
+                                        startup.win())
 
     def start(self):
         self._root_window.mainloop()
@@ -77,7 +73,7 @@ class OthelloApplication:
         """
         pass
 
-    def _validate_startup_parameters(self, dialog: StartupDialog) -> (str, list):
+    def _validate_startup_parameters(self, dialog: 'StartupDialog') -> (str, list):
         """
         Looks at the values stored in the StartupDialog object to see if they are all valid.
         :param dialog: StartupDialog object that has already been called and which the user is now finished with.
@@ -101,8 +97,42 @@ class StartupDialog:
 
         self._dialog_window = tkinter.Toplevel()
 
-        who_label = tkinter.Label(master=self._dialog_window, text='What kind of Othello game would you like to play?',
-                                  font=DEFAULT_FONT)
+        # Windows top title (not window 'bar' title)
+        prompt_label = tkinter.Label(master=self._dialog_window,
+                                     text='What kind of Othello game would you like to play?', font=TITLE_FONT)
+        prompt_label.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+        # Instructions for entering a row and column value
+        row_col_label_1 = tkinter.Label(master=self._dialog_window, text='Please specify the number of rows and '
+                                                                         'columns you would like to board to have.',
+                                        font=DEFAULT_FONT)
+        row_col_label_1.grid(row=1, column=0, columnspan=4, padx=10, pady=2, sticky=tkinter.W)
+        row_col_label_2 = tkinter.Label(master=self._dialog_window, text='Each value must be an even number between 4 '
+                                                                         'and 16 (inclusive).', font=DEFAULT_FONT)
+        row_col_label_2.grid(row=2, column=0, columnspan=4, padx=10, pady=2, sticky=tkinter.W)
+
+        # Row and column entry spaces with labels (both in the same row).
+        # Row label
+        rows_label = tkinter.Label(master=self._dialog_window, text='Rows:',
+                                   font=SUBTITLE_FONT)
+        rows_label.grid(row=3, column=0, padx=10, pady=5, sticky=tkinter.W)
+        # Row entry box
+        self._rows_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
+        self._rows_entry.grid(row=3, column=1, padx=10, pady=1, sticky=tkinter.W)
+        self._rows_entry.bind('<FocusIn>', self._field_get_focus)
+        self._rows_entry.bind('<FocusOut>', self._field_lose_focus)
+        self._rows_entry.is_valid = False
+
+        # Column label
+        cols_label = tkinter.Label(master=self._dialog_window,
+                                   text='Columns:', font=SUBTITLE_FONT)
+        cols_label.grid(row=3, column=2, padx=10, pady=5, sticky=tkinter.W)
+        # Column entry box
+        self._cols_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
+        self._cols_entry.grid(row=3, column=3, padx=10, pady=1, sticky=tkinter.W)
+        self._cols_entry.bind('<FocusIn>', self._field_get_focus)
+        self._cols_entry.bind('<FocusOut>', self._field_lose_focus)
+        self._cols_entry.is_valid = False
 
     def rows(self):
         return self._rows
@@ -122,6 +152,59 @@ class StartupDialog:
     def was_ok_clicked(self):
         return self._ok_clicked
 
+    def show(self):
+        self._dialog_window.grab_set()
+        self._dialog_window.wait_window()
+
+    def _on_ok_click(self):
+        self._ok_clicked = True
+        self._rows = self._rows_entry.get()
+        self._cols = self._cols_entry.get()
+        #first
+        #neplayer
+        #win
+
+        self._dialog_window. destroy()
+
+    def _on_quit_click(self):
+        self._dialog_window.destroy()
+        raise UserQuit
+
+    def _field_get_focus(self, event):
+        event.widget.config(background='#ffffff')
+
+    def _field_lose_focus(self, event):
+        event.widget.is_valid = self._validate_startup_widget(event.widget)
+        if event.widget.is_valid:
+            return
+        else:
+            event.widget.config(background='#ff8080')
+
+    def _validate_startup_widget(self, widget: 'tkinter widget') -> bool:
+        """
+        Takes one of the widgets from an active instance of StartupDialog and checks that is contains a valid value.
+        :param widget: A widget from an active instance of StartupDialog
+        :return: True or False
+        """
+
+        if widget == self._rows_entry or widget == self._cols_entry:
+            try:
+                value = int(widget.get())
+            except ValueError:
+                return False
+
+            if value % 2 != 0:
+                return False
+            elif not 4 <= value <= 16:
+                return False
+            else:
+                return True
+
+        raise UnableToIdentifyWidget
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        OthelloApplication().start()
+    except UserQuit:
+        pass
