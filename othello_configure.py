0f46e8e2254c43a07775d259be2bc292ae3fed4c
Dan_Laptop <tentativechaos@gmail.com>
2016-03-08 21:13:50 -0800

    -Created a working version of othello_configure. -Added a note about making variables 'self.' -Changed some things in othello_gui to handle othello_configure properly

---
+++ othello_configure.py
@@ -0,0 +1,220 @@
+import tkinter
+from othello_gui import DEFAULT_FONT
+from othello_gui import TITLE_FONT
+from othello_gui import SUBTITLE_FONT
+
+
+class UnableToIdentifyWidget(Exception):
+    """To be thrown when the tkinter widget specified cannot be identified as one of the expected types by a function"""
+
+
+class StartupDialog:
+
+    def __init__(self):
+        self._all_choices = {'rows': None, 'cols': None, 'first': None, 'ne_player': None, 'win': None}
+        self._all_choices_keys = ['rows', 'cols', 'first', 'ne_player', 'win']  # Order must match self._all_fields
+
+        self._ok_clicked = False
+
+        self._dialog_window = tkinter.Toplevel()
+
+        self._dialog_window.wm_title("[FULL] Options")
+
+        self._dialog_window.bind('<Configure>', self._dialog_window.after(10, self._dialog_window.lift))
+
+        # Windows top title (not window 'bar' title)
+        prompt_label = tkinter.Label(master=self._dialog_window,
+                                     text='What kind of Othello game would you like to play?', font=TITLE_FONT)
+        prompt_label.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky=tkinter.W+tkinter.E)
+
+        self._dialog_window.columnconfigure(0, weight=1)
+        self._dialog_window.columnconfigure(1, weight=1)
+        self._dialog_window.columnconfigure(2, weight=1)
+        self._dialog_window.columnconfigure(3, weight=1)
+        self._dialog_window.columnconfigure(4, weight=1)
+
+        # Instructions for entering a row and column value
+        row_col_label = tkinter.Label(master=self._dialog_window, text='Please specify the number of rows and '
+                                                                       'columns you would like to board to have.\n'
+                                                                       'Each value must be an even number between 4 '
+                                                                       'and 16 (inclusive).',
+                                      font=DEFAULT_FONT)
+        row_col_label.grid(row=1, column=0, columnspan=4, padx=10, pady=2, sticky=tkinter.W)
+
+        # Row and column entry spaces with labels (both in the same row).
+        # Row label
+        rows_label = tkinter.Label(master=self._dialog_window, text='Rows:', font=SUBTITLE_FONT)
+        rows_label.grid(row=3, column=0, padx=10, pady=5, sticky=tkinter.W)
+        # Row entry box
+        self._rows_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
+        self._rows_entry.grid(row=3, column=1, padx=10, pady=1, sticky=tkinter.W)
+        self._rows_entry.bind('<FocusIn>', self._field_get_focus)
+        self._rows_entry.bind('<FocusOut>', self._field_lose_focus)
+
+        # Column label
+        cols_label = tkinter.Label(master=self._dialog_window, text='Columns:', font=SUBTITLE_FONT)
+        cols_label.grid(row=3, column=2, padx=10, pady=5, sticky=tkinter.W)
+        # Column entry box
+        self._cols_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
+        self._cols_entry.grid(row=3, column=3, padx=10, pady=1, sticky=tkinter.W)
+        self._cols_entry.bind('<FocusIn>', self._field_get_focus)
+        self._cols_entry.bind('<FocusOut>', self._field_lose_focus)
+
+        # First player label
+        self._first_label = tkinter.Label(master=self._dialog_window,
+                                          text='Who goes first?', font=DEFAULT_FONT)
+        self._first_label.grid(row=5, column=0, columnspan=4, padx=10, pady=10)
+        # First player radio buttons
+        self._first_player = tkinter.IntVar()  # Value should be retrieved using the get() method
+
+        self._first_is_black = tkinter.Radiobutton(master=self._dialog_window, text='Black', variable=self._first_player,
+                                                   value=1, indicatoron=0, font=SUBTITLE_FONT, width=7)
+        self._first_is_black.grid(row=6, column=0, columnspan=2, padx=5, pady=0)
+        self._first_is_white = tkinter.Radiobutton(master=self._dialog_window, text='White', variable=self._first_player,
+                                                   value=2, indicatoron=0, font=SUBTITLE_FONT, width=7)
+        self._first_is_white.grid(row=6, column=2, columnspan=2, padx=5, pady=0)
+
+        # NE player label
+        self._ne_label = tkinter.Label(master=self._dialog_window,
+                                       text='Choose the starting layout of the board by specifying which player\'s\n'
+                                            'piece will appear in the North-West corner of the 4 central tiles.',
+                                       font=DEFAULT_FONT)
+        self._ne_label.grid(row=8, column=0, columnspan=4, padx=10, pady=10)
+        # NE player radio buttons
+        self._ne_player_selection = tkinter.IntVar()  # Value should be retrieved using the get() method
+
+        self._ne_is_black = tkinter.Radiobutton(master=self._dialog_window, text='Black', variable=self._ne_player_selection,
+                                                value=1, indicatoron=0, font=SUBTITLE_FONT, width=7)
+        self._ne_is_black.grid(row=9, column=0, columnspan=2, padx=5, pady=0)
+        self._ne_is_white = tkinter.Radiobutton(master=self._dialog_window, text='White', variable=self._ne_player_selection,
+                                                value=2, indicatoron=0, font=SUBTITLE_FONT, width=7)
+        self._ne_is_white.grid(row=9, column=2, columnspan=2, padx=5, pady=0)
+
+        # Win condition label
+        self._win_label = tkinter.Label(master=self._dialog_window, text='Please specify the desired victory '
+                                                                         'conditions:', font=DEFAULT_FONT)
+        self._win_label.grid(row=11, column=0, columnspan=4, padx=10, pady=10)
+        # Win radio buttons
+        self._win_condition = tkinter.BooleanVar()  # Value should be retrieved using the get() method
+
+        self._win_is_most = tkinter.Radiobutton(master=self._dialog_window, text='Most Pieces',
+                                                variable=self._win_condition, value=True, indicatoron=0,
+                                                font=SUBTITLE_FONT, width=14)
+        self._win_is_most.grid(row=12, column=0, columnspan=2, padx=5, pady=0)
+        self._win_is_least = tkinter.Radiobutton(master=self._dialog_window, text='Least Pieces',
+                                                 variable=self._win_condition, value=False, indicatoron=0,
+                                                 font=SUBTITLE_FONT, width=14)
+        self._win_is_least.grid(row=12, column=2, columnspan=2, padx=5, pady=0)
+
+        # List of all widget or variables in the class which take user input
+        self._all_fields = [self._rows_entry, self._cols_entry, self._first_player,
+                            self._ne_player_selection,  self._win_condition]  # Order must match self._all_choices_keys
+
+        # Set defaults
+        self._first_is_black.select()
+        self._ne_is_black.select()
+        self._win_is_most.select()
+        self._rows_entry.insert(tkinter.END, '8')
+        self._cols_entry.insert(tkinter.END, '8')
+
+        # Buttons
+        okay_button = tkinter.Button(master=self._dialog_window, text='Ok', font=SUBTITLE_FONT,
+                                     command=self._on_ok_click)
+
+        okay_button.grid(row=14, column=3, padx=10, pady=10, sticky=tkinter.S)
+
+        quit_button = tkinter.Button(master=self._dialog_window, text='Quit', font=SUBTITLE_FONT,
+                                     command=self._on_quit_click)
+
+        quit_button.grid(row=14, column=4, padx=10, pady=10, sticky=tkinter.S)
+
+    def rows(self):
+        return int(self._all_choices['rows'])
+
+    def cols(self):
+        return int(self._all_choices['cols'])
+
+    def first(self):
+        return int(self._all_choices['first'])
+
+    def ne_player(self):
+        return int(self._all_choices['ne_player'])
+
+    def win(self):
+        if self._all_choices['win']:
+            return '>'
+        else:
+            return '<'
+
+    def was_ok_clicked(self):
+        return self._ok_clicked
+
+    def validate_all(self):
+        for each in self._all_fields:
+            if not self._validate_startup_widget(each):
+                return False
+
+        return True
+
+    def show(self):
+        self._dialog_window.grab_set()
+        self._dialog_window.wait_window()
+
+    def _on_ok_click(self):
+
+        if self.validate_all():
+            for i in range(len(self._all_choices)):
+                self._all_choices[self._all_choices_keys[i]] = self._all_fields[i].get()
+
+            self._dialog_window.destroy()
+            self._ok_clicked = True
+
+        else:
+            self._dialog_window.focus_force()
+
+    def _on_quit_click(self):
+        self._dialog_window.destroy()
+        self._ok_clicked = False
+
+    def _field_get_focus(self, event):
+        event.widget.config(background='#ffffff')
+
+    def _field_lose_focus(self, event):
+        if self._validate_startup_widget(event.widget):
+            return
+        else:
+            event.widget.config(background='#ff8080')
+
+    def _validate_startup_widget(self, widget: 'tkinter widget') -> bool:
+        """
+        Takes one of the widgets from an active instance of StartupDialog and checks that is contains a valid value.
+        :param widget: A widget from an active instance of StartupDialog
+        :return: True or False
+        """
+
+        if widget is self._rows_entry or widget is self._cols_entry:
+            try:
+                value = int(widget.get())
+            except ValueError:
+                return False
+
+            if value % 2 != 0:
+                return False
+            elif not 4 <= value <= 16:
+                return False
+            else:
+                return True
+
+        elif widget is self._first_player or widget is self._ne_player_selection:
+            value = widget.get()
+
+            if value != 1 and value != 2:
+                return False
+            else:
+                return True
+        elif widget is self._win_condition:
+            value = widget.get()
+            return type(value) == bool
+
+        else:
+            raise UnableToIdentifyWidget(widget)
--- othello_gui.py
+++ othello_gui.py
@@ -5,9 +5,9 @@
 # and point.
 
 import othello_logic
+import othello_configure
 import point
 import tkinter
-
 
 DEFAULT_FONT = ('Helvetica', 12)
 TITLE_FONT = ('Helvetica', 18)
@@ -16,10 +16,6 @@ SUBTITLE_FONT = ('Helvetica', 16)
 
 class UserQuit(Exception):
     """To be thrown when the user elects to quit the program."""
-
-
-class UnableToIdentifyWidget(Exception):
-    """To be thrown when the tkinter widget specified cannot be identified as one of the expected types by a function"""
 
 
 class OthelloApplication:
@@ -35,33 +31,34 @@ class OthelloApplication:
 
         self._canvas.grid(row=0, column=0, padx=10, pady=10, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
 
-        #self._canvas.bind('<Configure>', self._on_canvas_resized)
+        self._canvas.bind('<Configure>', self._on_canvas_resized)
         self._canvas.bind('<Button-1>', self._on_canvas_clicked)
         self._canvas.bind('<Button-2>', self._on_canvas_right_clicked)
 
         self._root_window.rowconfigure(0, weight=1)
         self._root_window.columnconfigure(0, weight=1)
 
-        startup = StartupDialog()
+        startup = othello_configure.StartupDialog()
         startup.show()
 
-        if not startup.validate_all:
-            raise UserQuit("User x'd out of the startup dialogue.")
+        if not startup.was_ok_clicked():
+            raise UserQuit
 
         state = othello_logic.GameState(startup.rows(), startup.cols(), startup.first(), startup.ne_player(),
                                         startup.win())
 
+        self._root_window.lift()
+
     def start(self):
         self._root_window.mainloop()
 
-
-    def _on_canvas_resized(self):
+    def _on_canvas_resized(self, event: 'Tkinter Event'):
         pass
 
-    def _on_canvas_clicked(self):
+    def _on_canvas_clicked(self, event: 'Tkinter Event'):
         pass
 
-    def _on_canvas_right_clicked(self):
+    def _on_canvas_right_clicked(self, event: 'Tkinter Event'):
         pass
 
     def _redraw_all(self):
@@ -73,171 +70,6 @@ class OthelloApplication:
         canvas_height = self._canvas.winfo_height()
 
         #re-draw everything
-
-
-class StartupDialog:
-
-    def __init__(self):
-        self._rows = None
-        self._cols = None
-        self._first = None
-        self._ne_player = None
-        self._win = None
-
-        self._ok_clicked = False
-
-        self._dialog_window = tkinter.Toplevel()
-
-        # Windows top title (not window 'bar' title)
-        prompt_label = tkinter.Label(master=self._dialog_window,
-                                     text='What kind of Othello game would you like to play?', font=TITLE_FONT)
-        prompt_label.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky=tkinter.W+tkinter.E)
-
-        self._dialog_window.columnconfigure(0, weight=1)
-        self._dialog_window.columnconfigure(1, weight=1)
-        self._dialog_window.columnconfigure(2, weight=1)
-        self._dialog_window.columnconfigure(3, weight=1)
-        self._dialog_window.columnconfigure(4, weight=1)
-
-        # Instructions for entering a row and column value
-        row_col_label_1 = tkinter.Label(master=self._dialog_window, text='Please specify the number of rows and '
-                                                                         'columns you would like to board to have.',
-                                        font=DEFAULT_FONT)
-        row_col_label_1.grid(row=1, column=0, columnspan=4, padx=10, pady=2, sticky=tkinter.W)
-        row_col_label_2 = tkinter.Label(master=self._dialog_window, text='Each value must be an even number between 4 '
-                                                                         'and 16 (inclusive).', font=DEFAULT_FONT)
-        row_col_label_2.grid(row=2, column=0, columnspan=4, padx=10, pady=2, sticky=tkinter.W)
-
-        # Row and column entry spaces with labels (both in the same row).
-        # Row label
-        rows_label = tkinter.Label(master=self._dialog_window, text='Rows:',
-                                   font=SUBTITLE_FONT)
-        rows_label.grid(row=3, column=0, padx=10, pady=5, sticky=tkinter.W)
-        # Row entry box
-        self._rows_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
-        self._rows_entry.grid(row=3, column=1, padx=10, pady=1, sticky=tkinter.W)
-        self._rows_entry.bind('<FocusIn>', self._field_get_focus)
-        self._rows_entry.bind('<FocusOut>', self._field_lose_focus)
-        self._rows_entry.is_valid = False
-
-        # Column label
-        cols_label = tkinter.Label(master=self._dialog_window,
-                                   text='Columns:', font=SUBTITLE_FONT)
-        cols_label.grid(row=3, column=2, padx=10, pady=5, sticky=tkinter.W)
-        # Column entry box
-        self._cols_entry = tkinter.Entry(master=self._dialog_window, width=10, font=SUBTITLE_FONT)
-        self._cols_entry.grid(row=3, column=3, padx=10, pady=1, sticky=tkinter.W)
-        self._cols_entry.bind('<FocusIn>', self._field_get_focus)
-        self._cols_entry.bind('<FocusOut>', self._field_lose_focus)
-        self._cols_entry.is_valid = False
-
-        # First player label
-        self.first_label = tkinter.Label(master=self._dialog_window,
-                                         text='Who goes first?', font=DEFAULT_FONT)
-        self.first_label.grid(row=5, column=0, columnspan=4, padx=10, pady=10)
-        # First player radio buttons
-        first_player = tkinter.IntVar()  # Value should be retrieved using the get() method
-        self._first_is_black = tkinter.Radiobutton(master=self._dialog_window, text='Black', variable=first_player,
-                                                   value=1, indicatoron=0, font=SUBTITLE_FONT, width=7)
-        self._first_is_black.grid(row=6, column=0, columnspan=2, padx=5, pady=0)
-        self._first_is_white = tkinter.Radiobutton(master=self._dialog_window, text='White', variable=first_player,
-                                                   value=2, indicatoron=0, font=SUBTITLE_FONT, width=7)
-        self._first_is_white.grid(row=6, column=2, columnspan=2, padx=5, pady=0)
-
-        # NE player label
-        self.ne_label = tkinter.Label(master=self._dialog_window,
-                                      text='Choose the starting layout of the board by specifying which player\'s\n'
-                                           'piece will appear in the North-West corner of the 4 central tiles.',
-                                      font=DEFAULT_FONT)
-        self.ne_label.grid(row=8, column=0, columnspan=4, padx=10, pady=10)
-        # NE player radio buttons
-        ne_player = tkinter.IntVar()  # Value should be retrieved using the get() method
-        self._ne_is_black = tkinter.Radiobutton(master=self._dialog_window, text='Black', variable=ne_player,
-                                                value=1, indicatoron=0, font=SUBTITLE_FONT, width=7)
-        self._ne_is_black.grid(row=9, column=0, columnspan=2, padx=5, pady=0)
-        self._ne_is_white = tkinter.Radiobutton(master=self._dialog_window, text='White', variable=ne_player,
-                                                value=2, indicatoron=0, font=SUBTITLE_FONT, width=7)
-        self._ne_is_white.grid(row=9, column=2, columnspan=2, padx=5, pady=0)
-
-        # Dictionary of all widget or variables in the class which take user input
-        self._all_fields = {'rows': self._rows_entry, 'cols': self._cols_entry, 'first': first_player,
-                            'ne_player': ne_player}  # <-- INCOMPLETE!
-
-
-    def rows(self):
-        return self._rows
-
-    def cols(self):
-        return self._cols
-
-    def first(self):
-        return self._first
-
-    def ne_player(self):
-        return self._ne_player
-
-    def win(self):
-        return self._win
-
-    def was_ok_clicked(self):
-        return self._ok_clicked
-
-    def validate_all(self):
-        for each in self._all_fields:
-            if not self._validate_startup_widget(each):
-                return False
-
-        return True
-
-    def show(self):
-        self._dialog_window.grab_set()
-        self._dialog_window.wait_window()
-
-    def _on_ok_click(self):
-        self._ok_clicked = True
-        self._rows = self._rows_entry.get()
-        self._cols = self._cols_entry.get()
-        #first
-        #neplayer
-        #win
-
-        self._dialog_window. destroy()
-
-    def _on_quit_click(self):
-        self._dialog_window.destroy()
-        raise UserQuit
-
-    def _field_get_focus(self, event):
-        event.widget.config(background='#ffffff')
-
-    def _field_lose_focus(self, event):
-        event.widget.is_valid = self._validate_startup_widget(event.widget)
-        if event.widget.is_valid:
-            return
-        else:
-            event.widget.config(background='#ff8080')
-
-    def _validate_startup_widget(self, widget: 'tkinter widget') -> bool:
-        """
-        Takes one of the widgets from an active instance of StartupDialog and checks that is contains a valid value.
-        :param widget: A widget from an active instance of StartupDialog
-        :return: True or False
-        """
-
-        if widget == self._rows_entry or widget == self._cols_entry:
-            try:
-                value = int(widget.get())
-            except ValueError:
-                return False
-
-            if value % 2 != 0:
-                return False
-            elif not 4 <= value <= 16:
-                return False
-            else:
-                return True
-
-        raise UnableToIdentifyWidget
 
 
 if __name__ == '__main__':
